32f5f605089ce0d35f281fd87793987b0c652da0
ehanson8 <ehanson8@jhu.edu>
2018-09-20 15:18:36 -0400

    updates

--- samplingScript.py
+++ samplingScript.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import csv
 from datetime import datetime
+import itertools
 
 filePath = raw_input('Enter file path (C:/Test/): ')
 
@@ -11,25 +12,23 @@ f.writerow(['oldLocation']+['newLocation'])
 f2=csv.writer(open('unsampledLog'+datetime.now().strftime('%Y-%m-%d %H.%M.%S')+'.csv','wb'))
 f2.writerow(['oldLocation']+['newLocation'])
 
-print filePath
-
 for root, dirs, files in os.walk(filePath, topdown=True):
     print root
-    number = 1
-    while number < len(files):
-        oldLocation = os.path.join(root, files[number])
+    print files
+    sampledFiles = itertools.islice(files, 1, None, 10)
+    for sampledFile in sampledFiles:
+        print sampledFile
+        oldLocation = os.path.join(root, sampledFile)
         project = root[root.rfind('/')+1:]
         root1 = root[:root.rfind('/')]
         root2 = root1[:root1.rfind('/')]
         newLocation = os.path.join(root2, 'sampled', project)
         if not os.path.exists(newLocation):
-              os.makedirs(newLocation)
-              print newLocation
-        newLocation = os.path.join(newLocation, files[number])
+            os.makedirs(newLocation)
+        newLocation = os.path.join(newLocation, sampledFile)
         print oldLocation
         print newLocation
         f.writerow([oldLocation]+[newLocation])
-        number = number + 10
         shutil.move(oldLocation, newLocation)
     oldUnsampledLocation = os.path.join(root)
     if oldUnsampledLocation != filePath:
@@ -37,7 +36,5 @@ for root, dirs, files in os.walk(filePath, topdown=True):
         root1 = root[:root.rfind('/')]
         root2 = root1[:root1.rfind('/')]
         newUnsampledLocation = os.path.join(root2, 'unsampled', project)
-        print oldUnsampledLocation
-        print newUnsampledLocation
         f2.writerow([oldUnsampledLocation]+[newUnsampledLocation])
         shutil.move(oldUnsampledLocation, newUnsampledLocation)
